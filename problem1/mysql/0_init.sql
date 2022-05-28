67a5513b18df11d170c20d8cbf70ef0bea2a6e42
kkaneko-window_laptop <kkaneko@student.42tokyo.jp>
2022-05-29 03:23:14 +0900

    modified table structure

--- problem1/mysql/0_init.sql
+++ problem1/mysql/0_init.sql
@@ -1,20 +1,15 @@
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `name` varchar(64) DEFAULT '' NOT NULL,
   PRIMARY KEY (`id`)
 );
--- user1 user2
+
 CREATE TABLE `friend_link` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user1_id` int(11) NOT NULL,
-  `user2_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL
 );
--- user1 user2 block
+-- subject blocks object
 CREATE TABLE `block_list` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user1_id` int(11) NOT NULL,
-  `user2_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `subject_id` int(11) NOT NULL,
+  `object_id` int(11) NOT NULL
 );
